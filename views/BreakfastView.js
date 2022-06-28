7a1b6027d84da6ff50fb4bac9dadcf3337c78a13
olga <oklimovich37@gmail.com>
2022-06-28 10:41:10 +0200

    test

--- views/BreakfastView.js
+++ views/BreakfastView.js
@@ -1,105 +1,63 @@
 import { StyleSheet, 
          Text, 
-         View, 
          SafeAreaView, 
          Image, 
          TouchableOpacity, 
          ImageBackground, 
-         Dimensions,
-         ScrollView,
          FlatList,
          Modal,
-         Alert,
-         StatusBar,
          NativeEventEmitter} from 'react-native';
 import React, { useState, useEffect } from "react";
 import{Ionicons} from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import BreakfastList from '../RecipeLists/BreakfastList';
 import NewRecipeForm from '../components/NewRecipeForm';
 import Recipe from '../models/Recipe';
-//import { insert } from 'formik';
 import {findAll, insert, deleteById} from '../datebase/Dbutils';
-import { TextInput } from 'react-native';
-import MainView from './MainView';
 
 export default function BreakfastView({navigation}) {
 
-    const [recipe, setBreakfast]=useState(BreakfastList, []);
+    const [recipe, setBreakfast]=useState([]);
 
     const [modalWindow, setModalWindow]=useState(false);
     
     const emitter=new NativeEventEmitter()
 
-    /*const addRecipe=(recipe)=>{
-        setBreakfast((list)=>{
-            recipe.key=Math.random().toString();
-            return[
-                recipe,
-                ...list
-            ]
-        });
-        setModalWindow(false);
-    }*/
-
     const addRecipe=(recipe)=>{
-        //const recipe=new Recipe()
         insert (recipe)
-        .then(res=>{
+         .then(res=>{
             console.log("insert res", res)
             return findAll()
-        })
-        .then (res=>setBreakfast(res))
-        .catch(err=>console.log(err))
-        setBreakfast((list)=>{
+         })
+         .then (res=>setBreakfast(res))
+         .catch(err=>console.log(err))
+          setBreakfast((list)=>{
             recipe.key=Math.random().toString();
             return[
                 recipe,
                 ...list
             ]
-        });
-        setModalWindow(false);
-
+         });
+         setModalWindow(false);
    }
 
-    const deleteRecipe=(id)=>{
-        
-            deleteById(id)
-                //.then(res => emitter.emit('delete', recipe))
-                .then (res=>setBreakfast(res))
-                .catch(err=>console.log(err))
-                setBreakfast((list)=>{
-                    
-                    return[
-                       
-                        ...list
-                    ]
-        });
-       
-        
+   const deleteRecipe=(id)=>{
+     console.log("id" ,id)
+          deleteById(id)
+          .then(res=>findAll())
+          .then(res=>setBreakfast(res))
+          .catch (err=>console.log(err))    
     }
 
     const deleteListener=emitter.addListener('delete',(titel)=>{
         findAll()
-            .then(res => setBreakfast(res))
-            .catch(err => console.log(err))
-
+        .then(res=>setBreakfast(res))
+        .catch (err=>console.log(err)) 
     })
-    useEffect(() => {
+
+    useEffect(()=>{
         findAll()
-            .then(res => setBreakfast(res))
-        return () => deleteListener.remove()
-    }, [])
-
-    
-
-       
-        
-
-    
-
-
+        .then(res=>setBreakfast(res))
+        return()=>deleteListener.remove() 
+    },[])
 
     return (
       <SafeAreaView >
@@ -108,18 +66,21 @@ export default function BreakfastView({navigation}) {
                 source={require('../assets/breakfast.jpg')}>
                     <Text style={styles.headerText}>FRUKOST</Text>
 
-             <FlatList LisHeaderComponent={ <></>}
-                       data={recipe} renderItem={({item})=>(
+             <FlatList ListHeaderComponent={ <></>}
+                       data={recipe} 
+                       renderItem={({item})=>{
+                       console.log("item", item)
+                        return(
                          <TouchableOpacity style={styles.buttonView}
                             onPress={()=>navigation.navigate('Recipe',item)}>
                                 <Image style={styles.buttonImg} source={item.image}/>
                                 <Text style={styles.recipesTitle}>{item.titel}</Text>
                                 <Ionicons name='trash' 
                                           style={styles.iconDelete}
-                                          onPress={deleteRecipe}/>
-                         </TouchableOpacity>
-                         )}>
-                  ListFooterComponent={ <></>}  
+                                          onPress={()=>deleteRecipe(item.id)}/>
+                         </TouchableOpacity>)
+                         }}>
+                 ListFooterComponent={ <></>}
              </FlatList>
 
              <Ionicons name="add-circle" 
@@ -147,10 +108,6 @@ export default function BreakfastView({navigation}) {
         color: "#7e5e60",
         marginTop: 30,
         textAlign: "center",
-    },
-
-    scrollView: {
-
     },
 
     buttonView: {
@@ -196,8 +153,5 @@ export default function BreakfastView({navigation}) {
         fontSize: 25,   
     },
 
-    modal: {
-
-    },
 })
                 
